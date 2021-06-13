2ed054de4174a984712d80da884bb4e4434e65ae
eude313 <eudes.charles@student.moringaschool.com>
2021-06-13 12:48:29 +0300

    add created a function for names

---
+++ js/scrip.js
@@ -0,0 +1,14 @@
+// created a function for names
+function names(){
+    let = document.getElementById("birthday");
+    let sex = document.compareDocumentPosition.sex;
+    let dbir = birthday.value;    
+    let date = newDate(document.getElementById(birthday).value);
+    let theday = date.getDay();
+// created arrays that hold the akans name and weekdays
+    let week =  ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let female =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    let male = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    let valid = false; 
+
+}
