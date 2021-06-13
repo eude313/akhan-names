be21908b74567a7d9a3e1e9fb615efafb91811d9
eude313 <eudes.charles@student.moringaschool.com>
2021-06-13 12:54:26 +0300

    add using dom i called for the container from html

--- js/scrip.js
+++ js/scrip.js
@@ -5,10 +5,14 @@ function names(){
     let dbir = birthday.value;    
     let date = newDate(document.getElementById(birthday).value);
     let theday = date.getDay();
-// created arrays that hold the akans name and weekdays
+ // created arrays that hold the akans name and weekdays
     let week =  ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let female =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
     let male = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-    let valid = false; 
+    let valid = false;
+    
+    // using DOM method i called for the div container
+    let container = document.getElementById("container");
+    
 
 }
